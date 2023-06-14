45798dbeabeb0ed62439989f76779ab8e333f4f8
Emmanuel <nkememmanuel5505@gmail.com>
2023-06-14 17:13:59 +0100

    downlod

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Pdf_to_img.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Pdftoimg.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
