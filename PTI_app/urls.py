42435d812c4f838d763b7a9a19c2a1ace1fd244a
Emmanuel <nkememmanuel5505@gmail.com>
2023-06-15 22:43:23 +0100

    now_functional

--- PTI_app/urls.py
+++ PTI_app/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path("", views.upload_pdf, name="convert_pdf"),
-    path('download_images/<path:output_folder>/', views.download_images, name='download_images'),
 ]
--- PTI_app/views.py
+++ PTI_app/views.py
@@ -26,7 +26,7 @@ def upload_pdf(request):
         os.makedirs(output_path, exist_ok=True)  # Create directory if it doesn't exist
 
         # Save the PDF file
-        pdf_path = os.path.join(output_path, f'{output_folder}.pdf')
+        pdf_path = os.path.join(output_path, f'{output_folder}')
         with open(pdf_path, 'wb') as f:
             for chunk in pdf_file.chunks():
                 f.write(chunk)
@@ -49,41 +49,82 @@ def upload_pdf(request):
 
     elif request.method == 'GET':
         return render(request, 'PTI_app/home.html')
-from django.http import HttpResponse
-from django.conf import settings
-import os
-import zipfile
-import mimetypes
 
+
+import mimetypes
+import shutil
+import tempfile
 
 def download_images(request, output_folder):
+    # Sanitize the output folder name
+    output_folder = sanitize_filename(output_folder)
+
     # Define the directory path where the images are stored
     image_dir = os.path.join(settings.MEDIA_ROOT, 'pdf_images', output_folder)
 
-    # Get the first image file in the directory
-    image_file = next(iter(os.listdir(image_dir)), None)
+    # If the request specifies downloading as a folder
+    if request.GET.get('download_type') == 'folder':
+        # Create a temporary directory to hold the images
 
-    if image_file:
-        # Construct the path of the image file
-        image_path = os.path.join(image_dir, image_file)
+        temp_dir = tempfile.mkdtemp(dir=settings.MEDIA_ROOT)
+    
+    # Create a temporary folder to hold the images
+        temp_folder = os.path.join(temp_dir, output_folder)
+        os.makedirs(temp_folder, exist_ok=True)
 
-        # Open the image file in binary mode
-        with open(image_path, 'rb') as f:
-            # Read the image data
-            image_data = f.read()
+       
 
-        # Guess the MIME type of the image file
-        mime_type, _ = mimetypes.guess_type(image_file)
-        if mime_type is None:
-            mime_type = 'application/octet-stream'
+        # Copy the images to the temporary folder
+        for image_file in os.listdir(image_dir):
+            image_path = os.path.join(image_dir, image_file)
+            if os.path.isfile(image_path):
+                shutil.copy(image_path, temp_folder)
 
-        # Create a response with the image data
-        response = HttpResponse(image_data, content_type=mime_type)
+        # Get the MIME type of the first image file
+        first_image_file = os.listdir(temp_folder)[0]
+        first_image_path = os.path.join(temp_folder, first_image_file)
+        mime_type, _ = mimetypes.guess_type(first_image_path)
+
+        # Open the temporary folder as a file-like object
+        temp_folder_file = open(temp_folder, 'rb')
+
+        # Create a response to serve the temporary folder as a download
+        response = HttpResponse(temp_folder_file, content_type=mime_type)
 
         # Set the Content-Disposition header to specify the filename
-        response['Content-Disposition'] = f'attachment; filename="{image_file}"'
+        response['Content-Disposition'] = f'attachment; filename="{output_folder}"'
+
+        # Clean up the temporary directory
+        shutil.rmtree(temp_dir)
 
         return response
 
-    # If no image file is found, raise a 404 error
-    raise Http404("Image not found.")
+    # If the request specifies downloading individual files
+    elif request.GET.get('download_type') == 'files':
+        # Create a response to serve the image files
+        response = HttpResponse(content_type='image/jpeg')
+
+        # Iterate over the image files in the directory
+        for image_file in os.listdir(image_dir):
+            image_path = os.path.join(image_dir, image_file)
+            if os.path.isfile(image_path):
+                # Open the image file in binary mode
+                with open(image_path, 'rb') as f:
+                    # Read the image data
+                    image_data = f.read()
+
+                # Set the Content-Disposition header to specify the filename
+                response['Content-Disposition'] = f'attachment; filename="{image_file}"'
+
+                # Write the image data to the response
+                response.write(image_data)
+
+        # If no image file is found, raise a 404 error
+        if response.content == b'':
+            raise Http404("No images found.")
+
+        return response
+
+    # If the request does not specify a download type, return an error
+    else:
+        return HttpResponse("Invalid download type.")
